510e9396a06583ea2d76bb0f1d6160d85c1b210c
Herna Michel <labellehernamichel@yahoo.fr>
2019-07-05 14:23:14 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,5 +1,8 @@
 class SongsController < ApplicationController
 
+  before_action :set_song, only:[:show, :edit, :update, :destroy]
+
+
   def index
     @songs = Song.all
   end
@@ -27,24 +30,61 @@ class SongsController < ApplicationController
 
   def create
     @song = Song.new(song_params(:title, :artist_name, :genre, :release_year, :released))
+  end
+
+  def new
+    @song = Song.new
+  end
+
+  def create
+    @song = Song.new(song_params)
+
     if @song.valid?
       @song.save
       redirect_to song_path(@song)
     else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @song.assign_attributes(song_params)
+    if @song.valid?
+      @song.save
+      redirect_to song_path(@song)
+    else
+
       render new_song_path
+
+      render :edit
+
     end
   end
 
   def destroy
+
     @song = Song.find(params[:id])
-    @song.destroy
+
     redirect_to songs_path
   end
 
   private
 
+
   def song_params(*args)
     params.require(:song).permit(*args)
+  end
+
+
+  def set_song
+    @song = Song.find(params[:id])
+  end
+
+  def song_params
+    params.require(:song).permit(:title, :released, :release_year, :genre, :artist_name)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   resources :songs
+
    get '/', to: 'songs#index'
+
 end
